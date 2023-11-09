5300f1fbfc96893e98087191ceeeb5b70c2e0956
Seunggyo <tmdry99@gmail.com>
2023-11-09 15:43:07 +0900

    게시물 보기 페이지 작성

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ import {
 import { HomeLayout } from "./layout/HomeLayout";
 import { BoardWrite } from "./page/BoardWrite";
 import { BoardList } from "./page/BoardList";
+import { BoardView } from "./page/BoardView";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HomeLayout />}>
       <Route index element={<BoardList />} />
       <Route path="write" element={<BoardWrite />} />
+      <Route path="board/:id" element={<BoardView />} />
     </Route>,
   ),
 );
 
-function App(props) {
+function App() {
   return <RouterProvider router={routes} />;
 }
 
