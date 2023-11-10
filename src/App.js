acae2a9b650efbbb0104d5559142b6b0cf57a203
Seunggyo <tmdry99@gmail.com>
2023-11-10 16:29:34 +0900

    회원 가입자 명단 가져오기 기능 추가

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { BoardList } from "./page/board/BoardList";
 import { BoardView } from "./page/board/BoardView";
 import { BoardEdit } from "./page/board/BoardEdit";
 import { MemberSignup } from "./page/Member/MemberSignup";
+import { MemberList } from "./page/Member/MemberList";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -20,6 +21,7 @@ const routes = createBrowserRouter(
       <Route path="board/:id" element={<BoardView />} />
       <Route path="edit/:id" element={<BoardEdit />} />
       <Route path="signup" element={<MemberSignup />} />
+      <Route path="member/list" element={<MemberList />} />
     </Route>,
   ),
 );
---
+++ src/page/Member/MemberList.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import axios from "axios";
+
+export function MemberList() {
+  useEffect(() => {
+    axios.get("/api/member/list").then((r) => setList(r.data));
+  }, []);
+
+  const [list, setList] = useState(null);
+  if (list === null) {
+    return <Spinner />;
+  }
+  return (
+    <Box>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>id</Th>
+            <Th>password</Th>
+            <Th>email</Th>
+            <Th>가입 일시</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {list.map((member) => (
+            <Tr>
+              <Td>{member.id}</Td>
+              <Td>{member.password}</Td>
+              <Td>{member.email}</Td>
+              <Td>{member.inserted}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
+  );
+}
