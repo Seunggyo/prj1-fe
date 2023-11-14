2893620ee264c3dd8098e3db88a687e6719c9d75
Seunggyo <tmdry99@gmail.com>
2023-11-14 09:52:04 +0900

    로그인 로그아웃 기능 추가

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { BoardView } from "./page/board/BoardView";
 import { BoardEdit } from "./page/board/BoardEdit";
 import { MemberSignup } from "./page/Member/MemberSignup";
 import { MemberList } from "./page/Member/MemberList";
+import { MemberLogin } from "./page/Member/MemberLogin";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +23,7 @@ const routes = createBrowserRouter(
       <Route path="edit/:id" element={<BoardEdit />} />
       <Route path="signup" element={<MemberSignup />} />
       <Route path="member/list" element={<MemberList />} />
+      <Route path="login" element={<MemberLogin />} />
     </Route>,
   ),
 );
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,14 +1,22 @@
 import { Button, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export function NavBar() {
   const navigate = useNavigate();
+
+  function hadleLogout() {
+    axios.post("/api/member/logout").then(console.log("로그아웃"));
+  }
+
   return (
     <Flex>
       <Button onClick={() => navigate("/")}>home</Button>
       <Button onClick={() => navigate("/write")}>write</Button>
       <Button onClick={() => navigate("/signup")}>signup</Button>
       <Button onClick={() => navigate("/member/list")}>member list</Button>
+      <Button onClick={() => navigate("/login")}>login</Button>
+      <Button onClick={hadleLogout}>log out</Button>
     </Flex>
   );
 }
