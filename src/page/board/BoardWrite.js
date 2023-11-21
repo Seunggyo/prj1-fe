10c8437728a23fe1437de5e15a3f0ad45c7cf77d
Seunggyo <tmdry99@gmail.com>
2023-11-21 09:22:08 +0900

    파일 업로드 속성명 변경 ( files -> uploadFiles )

--- src/page/board/BoardWrite.js
+++ src/page/board/BoardWrite.js
@@ -15,7 +15,7 @@ import { useNavigate } from "react-router-dom";
 export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [files, setFiles] = useState(null);
+  const [uploadFiles, setUploadFiles] = useState(null);
 
   const toast = useToast();
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -27,7 +27,7 @@ export function BoardWrite() {
       .postForm("/api/board/add", {
         title,
         content,
-        files,
+        uploadFiles,
       })
       .then(() => {
         toast({
@@ -75,7 +75,7 @@ export function BoardWrite() {
             type="file"
             accept="image/*"
             multiple
-            onChange={(e) => setFiles(e.target.files)}
+            onChange={(e) => setUploadFiles(e.target.files)}
           />
           <FormHelperText>
             한 개 파일은 3MB, 총 용량은 10MB 이내로 첨부하세요.
