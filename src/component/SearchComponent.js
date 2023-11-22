9f39c2a3c48189b586df24199b032f40bd8c0c8d
Seunggyo <tmdry99@gmail.com>
2023-11-22 09:44:21 +0900

    검색 컴포넌트 다듬기

--- src/component/SearchComponent.js
+++ src/component/SearchComponent.js
@@ -1,4 +1,4 @@
-import { Button, Flex, Input, Select } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Input, Select } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -16,19 +16,31 @@ export function SearchComponent() {
   }
 
   return (
-    <Flex>
-      <Select
-        defaultValue={"all"}
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        <option value="all">제목+본문</option>
-        <option value="title">제목</option>
-        <option value="content">본문</option>
-      </Select>
-      <Input value={keyword} onChange={(e) => setKeyword(e.target.value)} />
-      <Button onClick={handleSearch}>
-        <SearchIcon />
-      </Button>
-    </Flex>
+    <Center mt={6}>
+      <Flex gap={2}>
+        <Box>
+          <Select
+            width={"200px"}
+            defaultValue={"all"}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="all">제목+본문</option>
+            <option value="title">제목</option>
+            <option value="content">본문</option>
+          </Select>
+        </Box>
+        <Box>
+          <Input
+            width={"400px"}
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+
+          <Button onClick={handleSearch}>
+            <SearchIcon />
+          </Button>
+        </Box>
+      </Flex>
+    </Center>
   );
 }
