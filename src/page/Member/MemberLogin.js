e136acbafcdd72efc76303e2b0d4df7fc000042c
Seunggyo <tmdry99@gmail.com>
2023-11-22 10:19:21 +0900

    login 화면 다듬기

--- src/page/Member/MemberLogin.js
+++ src/page/Member/MemberLogin.js
@@ -1,8 +1,13 @@
 import {
-  Box,
   Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Center,
   FormControl,
   FormLabel,
+  Heading,
   Input,
   useToast,
 } from "@chakra-ui/react";
@@ -38,23 +43,31 @@ export function MemberLogin() {
   }
 
   return (
-    <Box>
-      <h1>로그인</h1>
-      <FormControl>
-        <FormLabel>아이디</FormLabel>
-        <Input value={id} onChange={(e) => setId(e.target.value)} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>암호</FormLabel>
-        <Input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </FormControl>
-      <Button colorScheme="twitter" onClick={handleLogin}>
-        로그인
-      </Button>
-    </Box>
+    <Center>
+      <Card w={"md"}>
+        <CardHeader>
+          <Heading>로그인</Heading>
+        </CardHeader>
+        <CardBody>
+          <FormControl mb={5}>
+            <FormLabel>아이디</FormLabel>
+            <Input value={id} onChange={(e) => setId(e.target.value)} />
+          </FormControl>
+          <FormControl>
+            <FormLabel>암호</FormLabel>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormControl>
+        </CardBody>
+        <CardFooter>
+          <Button colorScheme="twitter" onClick={handleLogin}>
+            로그인
+          </Button>
+        </CardFooter>
+      </Card>
+    </Center>
   );
 }
